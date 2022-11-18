918ddd837d4881d6ed278f43dc0fa1183186b697
elekrisk <einar.vilhelm.persson@gmail.com>
2022-11-18 22:05:45 +0100

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+
+#include <SFML/Main.hpp>
+#include <SFML/Window.hpp>
+
+#include "world.hpp"
+#include "renderer.hpp"
+#include "player.hpp"
+
+int main()
+{
+    World world{};
+    world.add_entity(new Player{sf::Vector2i{1, 0}});
+
+    Renderer renderer{};
+
+    sf::RenderWindow &window = renderer.get_window();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        if (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case event.Closed:
+                window.close();
+                break;
+            default:
+                break;
+            }
+        }
+
+        renderer.render(world);
+    }
+
+    return 0;
+}
