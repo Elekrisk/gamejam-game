56aa579c03c72dc66e7249a498664fdb7a550493
elekrisk <einar.vilhelm.persson@gmail.com>
2022-11-19 14:57:47 +0100

    Render background and walls; add scaling factor

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,25 +1,69 @@
 #include "renderer.hpp"
+#include "asset_manager.hpp"
+#include <iostream>
 
-Renderer::Renderer()
+constexpr float TILE_SIZE = 16.0;
+constexpr float SCALE = 3.0;
+
+Renderer::Renderer() : background{}
 {
     new (&window) sf::RenderWindow(sf::VideoMode(800, 600), "Game");
+    sf::Texture *tile_texture = asset_manager.load<sf::Texture>("assets/tile.png");
+    tile_texture->setRepeated(true);
+    background.setTexture(*tile_texture, true);
+    background.setScale({SCALE, SCALE});
+    background.setTextureRect(sf::IntRect{0, 0, static_cast<int>(800 / SCALE), static_cast<int>(600 / SCALE)});
 }
 
-sf::RenderWindow& Renderer::get_window()
+sf::RenderWindow &Renderer::get_window()
 {
     return window;
 }
 
-void Renderer::render(World& world)
+bool once = true;
+
+void Renderer::render(World &world)
 {
     window.clear();
-    for (Entity* ent : world.get_entities())
+    window.draw(background);
+    for (Entity *ent : world.get_entities())
     {
-        sf::Sprite& sprite = ent->get_sprite();
+        sf::Sprite &sprite = ent->get_sprite();
         sf::Vector2f pos = static_cast<sf::Vector2f>(ent->get_position());
-        sprite.setPosition(pos * 16.0f);
-        
+        sprite.setPosition(pos * TILE_SIZE * SCALE);
+        sprite.setScale({SCALE, SCALE});
+
         window.draw(sprite);
     }
+    for (Wall const &wall : world.get_walls())
+    {
+        sf::RectangleShape rect{{TILE_SIZE / 8.0, TILE_SIZE}};
+        rect.setPosition((sf::Vector2f{wall.position} * TILE_SIZE + sf::Vector2f{TILE_SIZE / 2.0 + 1.0, TILE_SIZE / 2.0}) * SCALE);
+        rect.setScale({SCALE, SCALE});
+        rect.setOrigin({TILE_SIZE / 2.0 + 1.0, TILE_SIZE / 2.0});
+        switch (wall.direction)
+        {
+        case Wall::Direction::North:
+            rect.setRotation(90);
+            break;
+        case Wall::Direction::East:
+            rect.setRotation(180);
+            break;
+        case Wall::Direction::South:
+            rect.setRotation(270);
+            break;
+        case Wall::Direction::West:
+            break;
+        }
+        rect.setFillColor(sf::Color::Black);
+        if (once)
+        {
+            auto pos = rect.getPosition();
+            std::cout << pos.x << ", " << pos.y << std::endl;
+        }
+
+        window.draw(rect);
+    }
+    once = false;
     window.display();
 }
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -5,6 +5,7 @@
 
 class Renderer
 {
+    sf::Sprite background;
     sf::RenderWindow window;
 public:
     Renderer();
